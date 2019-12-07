90ad4f19803821550b20e6cd659271fc9688cbe5
Kaifi Asif <apple@Apples-MacBook-Pro.local>
2019-12-07 22:08:47 +0530

    Browrser Video  Added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-navigator.getUserMedia
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+// Select everything in my html
+const video = document.querySelector('#video');
+const audio = document.querySelector('#audio');
+const canvas = document.querySelector('#canvas');
+let context = canvas.getContext('2d');
+let modal;
+// Start Video 
+handTrack.startVideo(video)
+    .then(status => {
+        if (!status) {
+            navigator.getUserMedia({ video: {} }, stream => {
+                video.srcObject = stream;
+                setInterval(detection, 1000);
+            },
+                err => console.log(err)
+            );
+        }
+    })
+
+function detection() {
+    modal.detect(video)
+        .then(predictions => {
+            console.log("Predictions", predictions);
+        });
+}
+// Load Handtrack
+handTrack.load()
+    .then(lmodel => {
+        model = lmodel;
+    })
+
+
